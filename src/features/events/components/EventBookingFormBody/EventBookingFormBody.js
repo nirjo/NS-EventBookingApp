3506f496085b36512c9ebd8b4d6e17d85d870f74
Nirmalraj Joseph <nirmalraj.joseph86@gmail.com>
2020-10-25 18:33:31 +0530

    minor fix

--- src/features/events/components/EventBookingFormBody/EventBookingFormBody.js
+++ src/features/events/components/EventBookingFormBody/EventBookingFormBody.js
@@ -2,14 +2,27 @@ import React from 'react';
 
 import styles from './EventBookingFormBody.module.css';
 import EventBookingFormBodyInner from '../EventBookingFormBodyInner/EventBookingFormBodyInner';
+import styled from 'styled-components';
 
+
+const Wrapper = styled.section `
+  background: papayawhip;
+`;
+
+const Container = styled.div `
+   width: 1280px;
+   min-width: 1280px;
+   margin: 0 auto;
+`;
 const EventBookingFormBody = (props) => {
 	
 	return(
-
-  <div className={styles.EventBookingFormBody} data-testid="EventBookingFormBody">
-    <EventBookingFormBodyInner  event_item={props.event_item} />
-  </div>
+    <Wrapper>
+<Container>
+<EventBookingFormBodyInner  event_item={props.event_item} />
+</Container>
+</Wrapper>
+    
 );
 }
 
